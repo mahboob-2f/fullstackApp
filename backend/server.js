befd433d5f951a5e775757b6ee122dcc1dbc1836
mahboob alam <mahboob6550@gmail.com>
2025-10-05 23:40:28 +0530

    serever created

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Server is Ready");
+})
+
+//   get some jokes
+
+app.get('/jokes',(req,res)=>{
+    const jokes=[
+        {
+            id:1,
+            title:"A joke",
+            content:"this is a joke"
+        },
+        {
+            id:2,
+            title:"another joke",
+            content:" this is a another  joke"
+        },
+        {
+            id:3,
+            title:"third joke",
+            content:" this is third joke"
+        },
+        {
+            id:4,
+            title:"fourth joke",
+            content:"this is fourth joke"
+        },
+        {
+            id:5,
+            title:"fiveth joke",
+            content:"this is fiveth joke"
+        }
+    ]
+    res.send(jokes);
+})
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port,()=>{
+    console.log(`Serve at https://localhost:${port}`);
+})
